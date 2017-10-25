fdbce8c64cae5f1954ef624fe469d739383c1216
QJY <1092612943@qq.com>
2017-10-25 22:51:57 +0800

    add tool class FindUpdate

--- src/top/qiaojianyong/findcopy/FIndUpdate.java
+++ src/top/qiaojianyong/findcopy/FIndUpdate.java
@@ -11,51 +11,51 @@ import java.io.PrintWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FIndCopy {
+public class FIndUpdate {
 
 	// public static Pattern FILE_PATTERN = Pattern.compile("[\\\\/:*?\"<>|]");
 	//	private static Pattern KEY_PATTERN = Pattern.compile("\\w+:\\w+:\\w+");
-	private static Pattern KEY_PATTERN = Pattern.compile("MyLog.*$");
-	private static Matcher matcher;
-	private static String path = "C:\\_QJY\\MyDevelop\\MyIdea\\dt\\src\\main\\java\\top\\qiaojianyong\\dt";
+	private static String path = "C:\\_QJY\\MyDevelop\\MyJavaEE\\ttshop2";;
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("start");
-		PrintWriter print = new PrintWriter(
-				new OutputStreamWriter(new FileOutputStream(path + "\\keysfound.txt"), "utf-8"), true);
-		find(new File(path), print);
-		print.close();
+		System.out.println(path.replace("ttshop2", "ttshop2\\update"));
+		findUpdate(new File(path));
+		//print.close();
 		System.out.println("end");
 	}
 
 	//一边找一边打印,驴推磨
-	public static void find(File file, PrintWriter print) throws IOException {
+	public static void findUpdate(File file) throws IOException {
 		if (file != null)
 			if (file.isFile()) {
+				File newFile = new File(file.getAbsolutePath().replace("ttshop2", "ttshop2\\update"));
+				PrintWriter print = new PrintWriter(new OutputStreamWriter(new FileOutputStream(newFile), "utf-8"),
+						true);
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				String line;
 				boolean flag = false;
 				while ((line = br.readLine()) != null) {
-					matcher = KEY_PATTERN.matcher(line);
-					while (matcher.find()) {
-						flag = true;
-						print.println(matcher.group());
-					}
+					print.println(line.replace("qiaojianyong", "qjyoung"));
 				}
 				if (flag) {
-					print.println(file.getAbsolutePath());
-					print.println();
+					print.println(file.getName());
 				}
 				br.close();
+				print.close();
 			} else {
+				File newDir = new File(file.getAbsolutePath().replace("ttshop2", "ttshop2\\update"));
+				if (!newDir.exists()) {
+					newDir.mkdirs();
+				}
 				for (File f : file.listFiles(new FilenameFilter() {
-
 					@Override
 					public boolean accept(File dir, String name) {
-						return !name.equals("keysfound.txt"); // exclude
+						return !name.equals(".project") && !name.equals(".classpath") && !name.equals(".settings")
+								&& !name.equals("target") && !name.equals("update"); // exclude
 					}
 				})) {
-					find(f, print);
+					findUpdate(f);
 				}
 			}
 	}
